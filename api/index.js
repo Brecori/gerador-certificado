6cd102c275f3af543c70f907bacf976c0bfed217
Breno <brenotosi14@gmail.com>
2024-11-06 21:06:15 -0300

    feat: criando network no docker compose e passando para a fila

--- api/index.js
+++ api/index.js
@@ -1,43 +1,88 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const { Client } = require('pg');
-
+const express = require("express");
+const bodyParser = require("body-parser");
+const { Client } = require("pg");
+const amqp = require("amqplib");
 const app = express();
 
 app.use(bodyParser.json());
 
-
 const client = new Client({
-    user: 'admin',
-    password: 'root',
-    host: 'localhost',
-    port: 5432,
-    database: 'gerador_certificado'
-})
+  user: "postgres",
+  password: "postgres",
+  host: "postgres",
+  port: 5432,
+  database: "gerador_certificado",
+});
 
-app.post('/certificados', async (req, res) => {
-    const { nome_aluno, data_conclusao, nome_curso, nacionalidade, naturalidade, data_nascimento, numero_rg, data_emissao, diploma_url } = req.body;
+const sentToQueue = async (message) => {
+  try {
+    const connection = await amqp.connect("amqp://guest:guest@rabbitmq:5672");
+    const channel = await connection.createChannel();
 
-    if (!req.body) {
-        return res.status(400).send('Corpo da requisição vazio');
-    }
+    const queue = "teste";
 
-    const query = 'INSERT INTO certificados (nome_aluno, data_conclusao, nome_curso, nacionalidade, naturalidade, data_nascimento, numero_rg, data_emissao, diploma_url) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9)';
+    await channel.assertQueue(queue, { durable: true });
 
-    try {
-        await client.connect();
-        await client.query(query, [nome_aluno, data_conclusao, nome_curso, nacionalidade, naturalidade, data_nascimento, numero_rg, data_emissao, diploma_url]);
-        res.status(201).send('Certificado criado com sucesso');
-    }
-    catch (err) {
-        console.log('deu ruim', err);
-    }
-    finally {
-        await client.end();
-    }
-})
+    channel.sendToQueue(queue, Buffer.from(JSON.stringify(message)), {
+      persistent: true,
+    });
+
+    console.log("Mensagem enviada com sucesso", message);
+
+    // Fecha o canal e a conexão
+    await channel.close();
+    await connection.close();
+  } catch (error) {
+    console.error("Erro ao enviar mensagem para fila", error);
+  }
+};
+
+app.post("/certificados", async (req, res) => {
+  const {
+    nome_aluno,
+    data_conclusao,
+    nome_curso,
+    nacionalidade,
+    naturalidade,
+    data_nascimento,
+    numero_rg,
+    data_emissao,
+    diploma_url,
+  } = req.body;
+
+  if (!req.body) {
+    return res.status(400).send("Corpo da requisição vazio");
+  }
+
+  const query =
+    "INSERT INTO certificados (nome_aluno, data_conclusao, nome_curso, nacionalidade, naturalidade, data_nascimento, numero_rg, data_emissao, diploma_url) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9)";
+
+  try {
+    await client.connect();
+    await client.query(query, [
+      nome_aluno,
+      data_conclusao,
+      nome_curso,
+      nacionalidade,
+      naturalidade,
+      data_nascimento,
+      numero_rg,
+      data_emissao,
+      diploma_url,
+    ]);
+
+    sentToQueue(req.body);
+
+    await client.end();
+
+    res.status(201).send("Certificado criado com sucesso");
+  } catch (err) {
+    console.log("deu ruim", err);
+    await client.end();
+  }
+});
 
 const PORT = 3000;
 app.listen(PORT, () => {
-    console.log(`Servidor rodando na porta ${PORT}`);
+  console.log(`Servidor rodando na porta ${PORT}`);
 });
--- postgres/ddl.sql
+++ postgres/ddl.sql
@@ -1,8 +1,5 @@
-CREATE DATABASE IF NOT EXISTS gerador_certificado;
-USE gerador_certificado;
-
 CREATE TABLE IF NOT EXISTS certificados (
-    id INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
+    id SERIAL PRIMARY KEY,
     nome_aluno VARCHAR(255) NOT NULL,
     data_conclusao VARCHAR(255) NOT NULL,
     nome_curso VARCHAR(255) NOT NULL,
@@ -11,5 +8,5 @@ CREATE TABLE IF NOT EXISTS certificados (
     data_nascimento VARCHAR(255) NOT NULL,
     numero_rg VARCHAR(255) NOT NULL,
     data_emissao VARCHAR(255) NOT NULL,
-    diploma_url VARCHAR(255) 
-)
+    diploma_url VARCHAR(255)
+);
